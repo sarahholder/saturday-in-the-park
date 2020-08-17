ca24a1705bab0916f6ac82a2bfff5fc2ae47afaa
BryanNilsen <bryannilsen@gmail.com>
2020-08-17 15:25:31 -0500

    updated login/register/form to resolve errors

--- src/hooks/ui/useSimpleAuth.js
+++ src/hooks/ui/useSimpleAuth.js
@@ -8,7 +8,7 @@ const useSimpleAuth = () => {
         loggedIn || localStorage.getItem("kennywood_token") !== null
 
     const register = userInfo => {
-        return fetch("http://127.0.0.1:8000/register", {
+        return fetch("http://127.0.0.1:8000/register/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -19,14 +19,14 @@ const useSimpleAuth = () => {
             .then(res => res.json())
             .then(res => {
                 if ("token" in res) {
-                    localStorage.setItem( "kennywood_token", res.token )
+                    localStorage.setItem("kennywood_token", res.token)
                     setIsLoggedIn(true)
                 }
             })
     }
 
     const login = credentials => {
-        return fetch("http://127.0.0.1:8000/login", {
+        return fetch("http://127.0.0.1:8000/login/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -37,7 +37,7 @@ const useSimpleAuth = () => {
             .then(res => res.json())
             .then(res => {
                 if ("valid" in res && res.valid && "token" in res) {
-                    localStorage.setItem( "kennywood_token", res.token )
+                    localStorage.setItem("kennywood_token", res.token)
                     setIsLoggedIn(true)
                 }
             })
